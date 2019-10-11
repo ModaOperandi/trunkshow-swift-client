1be4e6e9cd9849eff206a236c514dbedd2ea0935
Ryan Martin <ryan.martin@modaoperandi.com>
2019-10-11 12:06:12 -0400

    Add host details

--- SwaggerClient/Classes/Swaggers/APIs.swift
+++ SwaggerClient/Classes/Swaggers/APIs.swift
@@ -7,7 +7,7 @@
 import Foundation
 
 open class SwaggerClientAPI {
-    open static var basePath = "/"
+    open static var basePath = "https://p6t3dy354h.execute-api.us-east-1.amazonaws.com/prod"
     open static var credential: URLCredential?
     open static var customHeaders: [String:String] = [:]
     open static var requestBuilderFactory: RequestBuilderFactory = AlamofireRequestBuilderFactory()
